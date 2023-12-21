e633282dd6f139f6fb5eacab454b7722f5f1edda
Matias1512 <matias.caturla@gmail.com>
2023-12-21 15:41:31 +0100

    Affichage de la liste de musique

---
+++ appli_music/lib/home_page/home_page.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:appli_music/home_page/album.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+Future<List<Album>> getAllAlbum(final String musicType) async {
+  final json = await http.get(Uri.parse(
+      'https://api.jamendo.com/v3.0/tracks/?client_id=1e6fe79c&format=jsonpretty&limit=10&fuzzytags=$musicType&include=musicinfo'));
+
+  List<Album> albums = [];
+
+  final jsDecode = jsonDecode(json.body) as Map<String, dynamic>;
+  for (var album in jsDecode['results']) {
+    albums.add(Album.fromJson(album));
+  }
+  return albums;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Variables pour suivre la sélection actuelle
+  String selectedAlbum = 'Album 1';
+  String selectedSong = 'Chanson 1';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(30.0),
+            child: FutureBuilder<List<Album>>(
+              future: getAllAlbum("jazz"),
+              // snapshot contient l'état de la future (terminée ou non)
+              builder: (context, snapshot) {
+                // True, si les données on bien été récupérer
+                if (snapshot.hasData) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: snapshot.data!
+                        .map((album) => Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child: Image.network(album.albumImage!)),
+                                  const Padding(padding: EdgeInsets.all(10)),
+                                  Expanded(
+                                      child: Column(children: [
+                                    Text(album.albumName!),
+                                    Text(album.artistName!)
+                                  ]))
+                                ],
+                              ),
+                            ))
+                        .toList(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

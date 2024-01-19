5c6c65e5c88d9178370d375214f21955f37a9017
kevin <kevinr12.mt@gmail.com>
2024-01-19 10:16:37 +0100

    delete comment

--- appli_music/lib/audioPlayer/audioplayer.dart
+++ appli_music/lib/audioPlayer/audioplayer.dart
@@ -1,20 +1,16 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class MyAudioPlayer extends AudioPlayer {
-  // Instance unique de la classe
+
   static MyAudioPlayer? _instance;
 
-  // Constructeur privé
   MyAudioPlayer._() : super();
 
-  // Méthode statique pour obtenir l'instance unique
   static MyAudioPlayer get instance {
     _instance ??=
-        MyAudioPlayer._(); // Crée l'instance si elle n'existe pas encore
+        MyAudioPlayer._(); 
     return _instance!;
   }
-
-  // Votre logique métier ici
 
   void playAudio(Source url) {
     play(url);
@@ -28,5 +24,5 @@ class MyAudioPlayer extends AudioPlayer {
     resume();
   }
 
-  // Ajoutez d'autres méthodes selon vos besoins
+
 }
